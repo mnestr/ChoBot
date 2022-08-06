eea72a9f166d8e4a0fbf6a35c4dca84df1329b78
Mikhail <3881625@gmail.com>
2022-08-06 18:31:57 +0300

    has changed bot token

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-token = '5532590680:AAH7jrLQjuDy87cFcin3UDM7uaD8DubYtEE'
+token = '5476157780:AAEWJRqDvXwqe-DjIDQXKsiV6TDvCjZDf24'
 database = "d4507rcebm77pe"
 user = 'yvobcmcbgkgweo'
 password = 'e88ed490b84655daef799b73606fadeb8bee8f41bf7f4b48654eb213c33fa71b'
