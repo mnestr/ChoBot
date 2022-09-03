e07599c70d4503991edca71a968836a224b4c4dd
Mikhail <3881625@gmail.com>
2022-09-03 18:07:43 +0300

    *in scheduler replaced buttons with base_buttons func *added before_repeat_word dialog

--- scheduler.py
+++ scheduler.py
@@ -1,21 +1,15 @@
 import telebot
 import db
 import config
-from telebot import types
-# import datetime
-# from pytz import timezone
+from main import base_buttons
 
 bot = telebot.TeleBot(config.token, parse_mode=None)
 
 if __name__ == "__main__":
     notify_list = db.get_notify_list()
     for item in notify_list:
-        # delta = timezone('UTC').localize(datetime.datetime.now()) - item[5]
         if not item[5] or item[3] > item[5]:
             db.count_notification(item[0])
-            markup = types.ReplyKeyboardMarkup(row_width=2)
-            answer_1 = types.KeyboardButton('Anything to repeat?')
-            answer_2 = types.KeyboardButton('Learn new words')
-            answer_3 = types.KeyboardButton('Add word')
-            markup.add(answer_1, answer_2, answer_3)
-            bot.send_message(item[1], "Hey, you have {0} words to repeat! Wanna start?".format(item[2]), reply_markup=markup)
+            markup = base_buttons()
+            bot.send_message(item[1], "Hey, you have {0} words to repeat! Wanna start?".format(item[2]),
+                             reply_markup=markup)
